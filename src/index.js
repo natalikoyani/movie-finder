d940c4b87457a28e4bcdbb2c1b3f9c4ef3ed1840
Natali Nesterenko <natalikagami@gmail.com>
2024-03-12 16:03:21 +0100

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { GlobalStyle } from 'components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/movie-finder">
       <App />
       <GlobalStyle />
     </BrowserRouter>
